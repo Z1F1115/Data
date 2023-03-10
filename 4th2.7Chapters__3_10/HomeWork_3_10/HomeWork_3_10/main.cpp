cfc2d681c6c5b14046be6dab63b6328067bd39c3
Z1F <F953364@163.com>
2023-03-11 00:10:18 +0800

    有序表的合并

---
+++ 4th2.7Chapters__3_10/HomeWork_3_10/HomeWork_3_10/main.cpp
@@ -0,0 +1,55 @@
+#include "SqList.h"
+
+//线性表的合并（合并后没有排序）
+void Union(SqList *& La, SqList * Lb) {
+	int La_len = ListLength(La);
+	int Lb_len = ListLength(Lb);
+	int i = 0;
+	int e = 0;
+	for (i = 1; i <= Lb_len; i++) {
+		GetElem(Lb, i, e);
+		if (!LocateElem(La, e)) ListInsert(La, ++La_len, e);
+	}
+}
+//时间复杂度O(ListLength(La)*ListLength(Lb))
+
+//有序表的合并
+void MergeList_Sq(SqList *La, SqList *Lb, SqList*& Lc) {
+	ElemType* pa = La->data;
+	ElemType* pb = Lb->data;
+	Lc->length = La->length + Lb->length;
+	Lc->data = new ElemType[Lc->length];
+	ElemType* pc = Lc->data;
+	ElemType* pa_last = La->data + La->length - 1;
+	ElemType* pb_last = Lb->data + Lb->length - 1;
+	while (pa <= pa_last && pb <= pb_last) {
+		if (*pa <= *pb) *pc++ = *pa++;
+		else *pc++ = *pb++;
+	}
+	while (pa <= pa_last) *pc++ = *pa++;
+	while (pb <= pb_last) *pc++ = *pb++;
+}
+//时间复杂度O(ListLength(La)+ListLength(Lb))
+//空间复杂度O(ListLength(La)+ListLength(Lb))
+
+int main()
+{
+	int a[] = { 1,2,3,5,7,9 };
+	int b[] = { 2,4,6,8,10 };
+	int sza = sizeof(a) / sizeof(a[0]);
+	int szb = sizeof(b) / sizeof(b[0]);
+	SqList* La;
+	SqList* Lb;
+	CreateList(La, a, sza);
+	CreateList(Lb, b, szb);
+
+	//线性表的合并（合并后没有排序）
+	Union(La,Lb);
+
+	//有序表的合并
+	SqList* Lc;
+	MergeList_Sq(La, Lb, Lc);
+
+	
+	return 0;
+}
